f88b8e97a555d6dd017b07982a8e0d53bd2d0de1
박한빈 <eekfkt@naver.com>
2025-01-15 17:16:36 +0900

    [Silver V] Title: 단어 정렬, Time: 552 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,12 +1,5 @@
 N = int(input())
-words = [input().strip() for _ in range(N)]
+words = list(set(input() for _ in range(N)))
 words.sort()
 words.sort(key=len)
-
-unique_words = []
-for word in words:
-    if word not in unique_words:
-        unique_words.append(word)
-
-for word in unique_words:
-    print(word)
+print('\n'.join(words))
