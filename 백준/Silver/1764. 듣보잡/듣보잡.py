c3d84af7a925d4cd7dc028bb9eb27f76c58f8e64
박한빈 <eekfkt@naver.com>
2025-06-26 08:38:39 +0900

    [Silver IV] Title: 듣보잡, Time: 72 ms, Memory: 37532 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,19 +1,20 @@
 import sys
+input = sys.stdin.readline
 
-N, M = map(int, sys.stdin.readline().split())
+# 입력: 듣도 못한 사람 수 N, 보도 못한 사람 수 M
+N, M = map(int, input().split())
 
-hnames = set()
-for _ in range(N):
-  hname = sys.stdin.readline().strip()
-  hnames.add(hname)
+# 듣도 못한 사람 명단을 set으로 저장
+heard = {input().strip() for _ in range(N)}
 
-snames = set()
+# 듣보잡 명단: 보도 못한 사람 중 듣도 못한 사람과 겹치는 이름
+result = []
 for _ in range(M):
-  sname = sys.stdin.readline().strip()
-  snames.add(sname)
+    name = input().strip()
+    if name in heard:
+        result.append(name)
 
-hsnames = sorted(hnames & snames)
-
-print(len(hsnames))
-for hsname in hsnames:
-  print(hsname)
+# 사전순 정렬 후 출력
+result.sort()
+print(len(result))
+print(*result, sep='\n')
