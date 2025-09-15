d6d8f153f497308d3bfc66562cab5b8f5fcf62f1
박한빈 <eekfkt@naver.com>
2025-09-16 00:46:37 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+char = input().strip()
+
+if char == 'M':
+  print('MatKor')
+elif char == 'W':
+  print('WiCys')
+elif char == 'C':
+  print('CyKor')
+elif char == 'A':
+  print('AlKor')
+else:
+  print('$clear')
