8e81c586a5c93f172804c19175f3392d6ce9680a
박한빈 <eekfkt@naver.com>
2025-10-08 03:55:34 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.33 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -4,19 +4,15 @@ def solution(wallpaper):
     행길이 = len(wallpaper)
     열길이 = len(wallpaper[0])
     
-    lux = math.inf
-    luy = math.inf
-    rdx = 0
-    rdy = 0
+    x = []
+    y = []
     
     for r in range(행길이):
         for c in range(열길이):
             if wallpaper[r][c] == '#':
-                lux = min(lux, r)
-                luy = min(luy, c)
-                rdx = max(rdx, r)
-                rdy = max(rdy, c)
+                x.append(r)
+                y.append(c)
     
     
-    answer = [lux, luy, rdx+1, rdy+1]
+    answer = [min(x), min(y), max(x)+1, max(y)+1]
     return answer
